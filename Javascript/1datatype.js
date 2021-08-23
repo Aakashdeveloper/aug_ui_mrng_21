6d21b6176634efd0b0373f15bc1217bc72f3f793
aakashdeveloper <ahanda205@gmail.com>
2021-08-23 09:51:24 +0530

    datatype

---
+++ Javascript/1datatype.js
@@ -0,0 +1,102 @@
+string = "Hiii" 'hiii' "test" 'test1' "345235" `hiiii`
+number = 87 897878 9878.344 .3534
+boolean = true/false
+
+//es5 >  Ecma Script 5
+//es6 >  Ecma Script 6
+
+// es5
+var a = "hii"
+var b = 10
+var c = true
+
+typeof(a)
+"string"
+typeof(b)
+"number"
+typeof(c)
+"boolean"
+var d = "hi10"
+typeof(d)
+"string"
+var e = 10.11
+typeof(e)
+"number"
+
+var a = 10
+var b = 20
+a+b (addition)
+30 
+a-b
+-10
+b-a
+10
+a*b
+200
+a/b
+
+4%2(modules)
+0
+4%3
+1
+
+2%4 = 2
+3%4 = 3
+4%4 = 0
+
+100%101 > 100
+
+
+var a = "hii"
+var b = 10
+a+b (concat)
+"hii10"
+a-b
+NaN (Not A Number)
+a*b
+NaN
+a/b
+NaN
+var b = "JavaScript"
+a+b (concat)
+"hiiJavaScript"
+
+string+ string  = string
+number+ string  = string
+string+ number  = string
+number+ number  = number
+
+10+"20"+30
+"1020"+30
+"102030"
+
+10+20+"30"
+30+"30"
+"3030"
+
+"10"+20+30
+"1020"+30
+"102030"
+
+10+"20"+30-1
+102029
+10+20+"30"-1
+3029
+"10"+20+30-1
+102029
+
+
+"hii"-1
+NaN
+"10"-1
+9
+"10"+1
+"101"
+"10"-1
+9
+"10a"-1
+NaN
+"10"*2
+20
+"10"/"2"
+5
---
+++ Javascript/2datatype.js
@@ -0,0 +1,82 @@
+var a = true;
+var b = true;
+a+b
+2
+var c = false;
+a+c
+1
+var d = false;
+c+d
+0
+
+true is equal to 1
+false is equal to 0
+
+10+true
+11
+10/false
+Infinity
+"true"+"true"
+"truetrue"
+"true"-"true"
+NaN
+true-true
+0
+
+
+"10"+1
+
+
+"10"+1
+"101"
+var a = "10"
+var b = "10.221"
+parseInt(a)
+10
+parseInt(b)
+10
+parseInt(a)+parseInt(b)
+20
+parseFloat(b)
+10.221
+parseInt(a)+parseFloat(b)
+20.221
+parseFloat(a)
+10
+
+var a = "hiii"
+parseInt(a)
+NaN
+var a = 10
+a.toString()
+"10"
+
+var a = "123delhi456"
+var b = "123456delhi"
+var c = "delhi123456"
+
+parseInt(a)
+123
+parseInt(b)
+123456
+parseInt(c)
+NaN
+
+//es6
+Number
+
+var a = "10"
+var b = "10.221"
+Number(a)
+10
+Number(b)
+10.221
+var c = "Hiii"
+Number(c)
+NaN
+var d = "Hiii10"
+Number(d)
+NaN
+var d = "10Hiii10"
+Number(d)
+NaN
