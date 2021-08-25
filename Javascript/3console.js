965bfa28b5c85e9a56c7cdc6916e221f207c893d
aakashdeveloper <ahanda205@gmail.com>
2021-08-25 10:00:56 +0530

    math

---
+++ Javascript/3console.js
@@ -0,0 +1,32 @@
+console.log() > for checking output and debugging
+alert() > for notification
+prompt() > for take input from user
+confirm() > to check weather yes or no
+
+alert("No Slot Open")
+undefined
+alert("Registration successfull")
+undefined
+confirm("Do you want to quit")
+true
+confirm("Do you want to quit")
+false
+var out = confirm("Do you want to quit")
+undefined
+out
+true
+var out = confirm("Do you want to quit")
+undefined
+out
+false
+
+prompt("what is your name")
+"Aakash"
+var age = prompt("what is your age")
+undefined
+age
+"10"
+
+var a = prompt("Enter First Number")
+var b = prompt("Enter Second Number")
+alert(Number(a)+Number(b))
---
+++ Javascript/4operators.js
@@ -0,0 +1,78 @@
+= // assignment
+== // compare the data
+=== // compare the data and datatype
+
+var a = 20  >>>> assignment
+var b = "20"
+
+a == b (compare the data)
+true
+a===b  (compare the data and datatype)
+false
+
+
+var a = "John"
+var b = "john"
+(case sensitive)
+a == b  
+false
+a === b
+false
+
+
+var a = true
+var b = "true"
+
+a==b
+false
+a===b
+false
+
+var a = 20
+var b = 10
+a>b
+true
+a<b
+false
+a==b
+false
+a !=b 
+true
+a !== b
+true
+
+// negation
+var a = 1
+!a
+
+
+var a = true
+undefined
+!a
+false
+var a = 1
+undefined
+!a
+false
+var b = 0
+!b
+true
+var a = -1
+
+!a
+false
+var a = "hii"
+!a
+false
+
+truthy 
+> any number except 0 weather +ve or -ve 
+> true
+> string
+
+
+falsy
+> 0
+> null
+> false
+> undefined
